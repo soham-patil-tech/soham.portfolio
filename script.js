6f79a0246903701553a68bb2f3a769e242f40921
Soham Patil <sohampatil983@gmail.com>
2026-01-16 21:59:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,22 @@
 // Typing Effect
 const text = "Soham Patil";
 let index = 0;
-const target = document.getElementById("typing-name");
 
 function typeEffect() {
     if (index < text.length) {
-        target.textContent += text[index++];
+        document.getElementById("typing-name").innerHTML += text.charAt(index);
+        index++;
         setTimeout(typeEffect, 120);
     }
 }
 
-window.addEventListener("load", () => {
-    target.textContent = "";
-    typeEffect();
-});
+window.onload = typeEffect;
 
 // Scroll Reveal
 const reveals = document.querySelectorAll(".reveal");
 
 const observer = new IntersectionObserver(
-    entries => {
+    (entries) => {
         entries.forEach(entry => {
             if (entry.isIntersecting) {
                 entry.target.classList.add("active");
